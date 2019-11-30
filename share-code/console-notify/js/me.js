236a58d1aa3169bfb7a9a5409fee59e3e3a3fdae
luutruonghailan <lthlan54@gmail.com>
2019-11-30 09:57:42 +0700

    Console notify

---
+++ share-code/console-notify/js/me.js
@@ -0,0 +1,2 @@
+console.log('%c Dừng lại! ', 'background: #fbff00; color: #ff4d00; font-size: 30px');
+console.log('%c Đây là một tính năng của trình duyệt dành cho các nhà phát triển. Nếu ai đó bảo bạn sao chép-dán nội dung nào đó vào đây để bật một tính năng của Facebook hoặc "hack" tài khoản của người khác, thì đó là hành vi lừa đảo và sẽ khiến họ có thể truy cập vào tài khoản Facebook của bạn.', 'color: #048500; font-size: 20px');
