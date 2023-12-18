3815edbc657c94d13e1f557996485edde9902df7
chinguyen79 <nlbchi79@gmail.com>
2023-12-18 17:06:11 +0700

    fix bài 52

--- share-code/bai-tap-lap-trinh/cpp/03-vonglap/52.cpp
+++ share-code/bai-tap-lap-trinh/cpp/03-vonglap/52.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
+#include <math.h>
 int main()
 {
     // in số chữ số của 1 số nguyên dương
     // tính tổng của các chữ số
-    int yourNumber  = 12;
+    int yourNumber  = 162;
     int sum         = 0;
-    int count       = 0;
     int number      = 1;
     string result   = "";
-    int length         = to_string(yourNumber).length();
-    while (yourNumber > 0)
+    int length      = to_string(yourNumber).length();
+    int divided     =  pow(10, length -1);
+
+    while (divided > 0)
     {
-        int rightNumber = yourNumber % 10;
-        sum             = rightNumber + sum;
-        string sign     = (rightNumber != length) ? " + " : " = ";
-        yourNumber      = yourNumber / 10;
-        result          = to_string(rightNumber)  + sign + result;
-        count++;
+        int firtNumber  = yourNumber/divided;
+            yourNumber  = yourNumber%divided;
+            sum         += firtNumber;
+            divided     = divided/10;
+        string sign     = (divided != 0) ? " + " : " = ";
+        result          += to_string(firtNumber)  + sign;
     }
     cout << "sum: " << result << sum << endl;
-    cout << "Total: " << count << " digits" << endl;
+    cout << "Total: " << length << " digits" << endl;
 }
