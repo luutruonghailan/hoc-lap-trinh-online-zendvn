9c6b1d56fae97d09ab78b22f9d4e9ed9ec35e94b
chinguyen79 <nlbchi79@gmail.com>
2023-12-18 19:35:46 +0700

    fix hinh 13

--- share-code/bai-tap-lap-trinh/cpp/05-thaphinh/13.cpp
+++ share-code/bai-tap-lap-trinh/cpp/05-thaphinh/13.cpp
@@ -3,26 +3,32 @@ using namespace std;
 int main()
 {
     /*
-    ====+
-    ===++
-    ==+++
-    =++++
     +++++
+    =++++
+    ==+++
+    ===++
+    ====+
     */
-    int number = 3;
-    while (number >= 1)
-    {
-        for (int j = 1; j <= 3-number; j++)
-        {
-            cout << "=";
-        }
-        for (int i = 1; i <= number; i++)
-        {
-            cout << "+";
-        }
-        
-        cout << "\n";
 
-        number--;
+    int number          = 1;
+    const int HEIGHT    = 5;
+    for (int i = HEIGHT; i >= number; i--)
+    {
+        string charater      = "";
+        string mark          = "";
+        string shape         = "";
+        string record        = "";
+        for (int j = 1; j <= HEIGHT-i; j++)
+        {
+            charater += "=";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            mark += "+";
+        }
+
+        record = charater + mark;
+        shape  += record;
+        cout << shape << "\n";
     }
 }
