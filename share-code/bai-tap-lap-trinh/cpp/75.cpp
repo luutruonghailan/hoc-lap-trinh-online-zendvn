702b1ac9e214594d760dca2d5bb62b2e85fc951c
chinguyen79 <nlbchi79@gmail.com>
2023-12-20 00:59:43 +0700

    fix 75

--- share-code/bai-tap-lap-trinh/cpp/75.cpp
+++ share-code/bai-tap-lap-trinh/cpp/75.cpp
@@ -5,37 +5,29 @@ using namespace std;
 
 int main()
 {
-    // Còn sai output
     string str = "luutruonghailan";
     string key = "3633";
-
-    
     string result = "";
     int startIndex = 0;
     int endIndex = 0;
-
+    
     for (int i = 0; i < key.length(); i++)
     {
         string word = "";
         int current = key[i] - '0';
-        endIndex = startIndex + current;
-        
-        word = str.substr(startIndex, endIndex) + " ";
-
-        
+        word = str.substr(endIndex,current) + " ";
         string upCase = word.substr(0, 1);
         for (char &c : upCase)
         {
             c = std::toupper(c);
         }
         word = upCase + word.substr(1);
-        
-
         result += word;
         startIndex = endIndex;
+        endIndex = startIndex + current;
     }
 
-    // cout << "Output: " << result << endl;
+    cout << "Output: " << result << endl;
 
     return 0;
 }
