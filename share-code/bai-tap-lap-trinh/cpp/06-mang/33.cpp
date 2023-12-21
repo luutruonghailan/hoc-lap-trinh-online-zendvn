7261126b5a4a692460a928051c843ac14ccc779e
Linh Nguyen <nvlinh17041992@gmail.com>
2023-12-21 19:33:17 +0700

    33

--- share-code/bai-tap-lap-trinh/cpp/06-mang/33.cpp
+++ share-code/bai-tap-lap-trinh/cpp/06-mang/33.cpp
@@ -5,20 +5,24 @@ using namespace std;
 
 int main()
 {
-    int arrNumber[] = {1,2,3};
+    int arrNumber[] = {1, 2, 7, 9, 2};
     bool found;
     int length = sizeof(arrNumber) / sizeof(int);
 
-    for (int i = 0; i < length; i++)
+    for (int i = 1; i < length - 1; i++)
     {
-        if (arrNumber[i] == arrNumber[i-1] + arrNumber[i+1])
+        if (arrNumber[i] == arrNumber[i - 1] + arrNumber[i + 1])
         {
             found = true;
-            cout << "Tồn tại";
         }
     }
+
     if (found != true)
     {
         cout << "Không tồn tại";
+    }
+    else
+    {
+        cout << "Tồn tại";
     }
 }
