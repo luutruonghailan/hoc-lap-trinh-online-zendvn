e37890679738bd77466f68962d42c977554ee5a2
Linh Nguyen <nvlinh17041992@gmail.com>
2023-12-21 15:32:05 +0700

    18

--- share-code/bai-tap-lap-trinh/cpp/05-thaphinh/18.cpp
+++ share-code/bai-tap-lap-trinh/cpp/05-thaphinh/18.cpp
@@ -1,64 +1,44 @@
 #include <iostream>
 
 using namespace std;
-int main()
+
+string myRepeat(string character, int length)
 {
-    /*
-     * * * * * *   11 12 13 14 15 16
-             *     21 22 23 24 25 26  n=2   i=5  2
-          *        31 32 33 34 35 36  n=3   i=4  1
-        *          41 42 43 44 45 46  n=4   i=3  -1
-      *            51 52 53 54 55 56  n=5   i=2  -2
-    * * * * * *
-    */
-    int number = 1;
-    
-
-    while (number <= 6)
+    string record = "";
+    for (int i = 1; i <= length; i++)
     {
-        
-        for (int i = 1; i <= 6; i++)
-        {
-            if (number == 1  || number == 6)
-            {
-                cout << "*" ;
-            }
-            if (number == 2 && i == 5)
-            {
-                cout << "    " << "*" ;
-            }
-            
-            if (number == 3 && i == 4)
-            {
-                 cout << "   " << "*" ;
-            }
-            if (number == 4 && i == 2)
-            {
-                 cout << "  " << "*" ;
-            }
-             if (number == 5 && i == 1)
-            {
-                 cout << " " << "*" ;
-            }
-        }
-       
-    cout  << "\n";
-        // cout <<space << number<< space << "\n";
-
-        number++;
+        record += character;
     }
+    return record;
 }
 
-//  if i == 0 or i == height - 1 or j == 0 or j == height - 1 or i == j or j == height - 1 - i:
-//             result += "*"
-//         else:
-//             result += " "
-/*
-*****
+int main()
+{
+    // * * * * * *
+    //         *
+    //       *
+    //     *
+    //   *
+    // * * * * * *
 
+    int height = 6;
+    string shape = "";
 
+    for (int i = 1; i <= height; i++)
+    {
+        string record = "";
 
-*****
+        if (i == 1 || i == height)
+        {
+            record = myRepeat("* ", height);
+        }
+        else
+        {
+            record = myRepeat("  ", height - i) + "* ";
+        }
 
+        shape += record + "\n";
+    }
 
-*/
+    cout << shape << endl;
+}
