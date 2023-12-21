7df30a9c7ff9a5f30e199acbf4ae2864a8656cb2
Linh Nguyen <nvlinh17041992@gmail.com>
2023-12-21 19:36:43 +0700

    58

--- share-code/bai-tap-lap-trinh/cpp/06-mang/58.cpp
+++ share-code/bai-tap-lap-trinh/cpp/06-mang/58.cpp
@@ -7,22 +7,20 @@ int main()
     int arrInt[] = {1, 2, 3};
     int sumEven = 0;
     int sumOld = 0;
-    int sumAll = 0;
-    int length = sizeof(arrInt) / sizeof(int);
 
-    for (int i = 0; i < length; i++)
+    for (int number : arrInt)
     {
-        if (arrInt[i] % 2 == 0)
+        if (number % 2 == 0)
         {
-            sumEven += arrInt[i];
+            sumEven += number;
         }
         else
         {
-            sumOld += arrInt[i];
+            sumOld += number;
         }
-        sumAll += arrInt[i];
     }
-    cout << "sumEven: " << sumEven << endl;
-    cout << "sumOld: " << sumOld << endl;
-    cout << "sumAll: " << sumAll << endl;
+
+    cout << "sumEven = " << sumEven << endl;
+    cout << "sumOld = " << sumOld << endl;
+    cout << "sumAll = " << sumEven + sumOld << endl;
 }
