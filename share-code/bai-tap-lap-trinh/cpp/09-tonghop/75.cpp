3bd099de30991e2803b01860f7f4d914f1df32a7
Linh Nguyen <nvlinh17041992@gmail.com>
2023-12-25 18:57:47 +0700

    75

--- share-code/bai-tap-lap-trinh/cpp/09-tonghop/75.cpp
+++ share-code/bai-tap-lap-trinh/cpp/09-tonghop/75.cpp
@@ -9,22 +9,14 @@ int main()
     string key = "3633";
     string result = "";
     int startIndex = 0;
-    int endIndex = 0;
-    
+
     for (int i = 0; i < key.length(); i++)
     {
-        string word = "";
-        int current = key[i] - '0';
-        word = str.substr(endIndex,current) + " ";
-        string upCase = word.substr(0, 1);
-        for (char &c : upCase)
-        {
-            c = toupper(c);
-        }
-        word = upCase + word.substr(1);
+        int size = key[i] - '0';
+        string word = str.substr(startIndex, size) + " ";
+        word[0] = toupper(word[0]);
         result += word;
-        startIndex = endIndex;
-        endIndex = startIndex + current;
+        startIndex += size;
     }
 
     cout << result << endl;
