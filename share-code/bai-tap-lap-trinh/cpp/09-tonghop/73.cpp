1bf919d9d0a19dfacd728d2f605b04defcdc344e
Linh Nguyen <nvlinh17041992@gmail.com>
2023-12-25 17:31:35 +0700

    73-74

--- share-code/bai-tap-lap-trinh/cpp/09-tonghop/73.cpp
+++ share-code/bai-tap-lap-trinh/cpp/09-tonghop/73.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
-#include <string>
-#include <sstream>
+
 using namespace std;
 
-int main() {
+int main()
+{
     string id = "69";
-    string str = "1, 69";
-    stringstream ss(str);
-    string token;
+    string str = "69,  1,   69 , 169 ";
+    // 69,  1,   69 , 169 ,
+    // ,   69,  1,   69 , 169
+    string strNoSpace = "";
+    string strId = "";
     int count = 0;
-    
-    while (getline(ss, token, ',')) {
-        size_t start = token.find_first_not_of(" ");
-        size_t end = token.find_last_not_of(" ");
-        
-        if (start != string::npos && end != string::npos) {
-            token = token.substr(start, end - start + 1);
-            if (token == id) {
-                count++;
-            }
+
+    // remove space
+    for (char character : str)
+    {
+        if (!isspace(character))
+        {
+            strNoSpace += character;
         }
     }
-    cout << "ID " << id << " xuất hiện " << count << " lần trong chuỗi." << endl;
 
-    return 0;
+    strNoSpace += ",";
+
+    int length = strNoSpace.length();
+    cout << "strNoSpace: " << strNoSpace << endl;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (strNoSpace[i] == ',')
+        {
+            if (id.compare(strId) == 0)
+            {
+                count++;
+            }
+            strId = "";
+            continue;
+        }
+        else
+        {
+            strId += strNoSpace[i];
+        }
+    }
+
+    cout << "ID " << id << " xuất hiện " << count << " lần";
 }
--- share-code/bai-tap-lap-trinh/cpp/09-tonghop/74.cpp
+++ share-code/bai-tap-lap-trinh/cpp/09-tonghop/74.cpp
@@ -1,31 +1,64 @@
 #include <iostream>
-#include <sstream>
-#include <string>
+#include <vector>
+
 using namespace std;
 
+bool isNumber(string inputStr)
+{
+    try
+    {
+        stoi(inputStr);
+        return true;
+    }
+    catch (const std::exception &e)
+    {
+        return false;
+    }
+}
 
-int main() {
-   
-    string str1 = "php/12";
-    istringstream stream(str1);
-    string courseName, timeStr;
+int main()
+{
+    // string str = "php/12/java/140/frontend/30/javascript/15";
+    string str = "php/12/java/140/frontend/30/javascript/15/python/140";
+    int strLength = str.length();
+    vector<string> vInfo;
+    string item = "";
+    bool isSlash = false;
+    int maxTime = -1;
+    int maxTimeIdx = -1;
 
-    int maxTime = 0;
-    string longestCourse;
-
-    while (getline(stream, courseName, '/')) {
-        if (!getline(stream, timeStr, '/')) {
-            break; 
+    for (int i = 0; i < strLength; i++)
+    {
+        char character = str[i];
+        if (character == '/')
+        {
+            isSlash = true;
         }
-        
-        int time = stoi(timeStr);
-        if (time > maxTime) {
-            maxTime = time;
-            longestCourse = courseName;
+        else
+        {
+            item += character;
+        }
+
+        if (isSlash || i == strLength - 1)
+        {
+            if (isNumber(item) && stoi(item) > maxTime)
+            {
+                maxTime = stoi(item);
+                maxTimeIdx = vInfo.size();
+            }
+            vInfo.push_back(item);
+            isSlash = false;
+            item = "";
         }
     }
 
-    cout << longestCourse << ": " << maxTime << endl;
+    for (int i = maxTimeIdx; i < vInfo.size(); i = i + 2)
+    {
+        if (stoi(vInfo[i]) == maxTime)
+        {
+            cout << vInfo[i - 1] << ": " << maxTime << endl;
+        }
+    }
 
     return 0;
 }
