026d5afc03ef145d57063afc51747595b8be9619
QUY PHAM <quypham252@gmail.com>
2025-06-14 12:31:05 +0700

    source java taphoa

---
+++ share-code/pvquy/source-start-taphoa/src/main/java/com/example/taphoa/TaphoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.taphoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaphoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaphoaApplication.class, args);
+	}
+
+}
---
+++ share-code/pvquy/source-start-taphoa/src/test/java/com/example/taphoa/TaphoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taphoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaphoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
